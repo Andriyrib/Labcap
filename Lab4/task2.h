3f0bbb28e3b6d463b9c733d304522be2b4b2dda9
Andriyrib <145540953+Andriyrib@users.noreply.github.com>
2023-12-13 15:25:21 +0200

    Protsan Andriy 2 year compmath1 lab2
    Classwork/Homework

---
+++ Lab4/task2.h
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    printf("%d! = 1", n);
+    for (int i = 2; i <= n; i++) {
+        printf("*%d", i);
+    }
+    printf("\n%d! = %d", n, n);
+    for (int i = n - 1; i >= 1; i--) {
+        printf("*%d", i);
+    }
+    putchar('\n');
+}
---
+++ Lab4/task4.h
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+
+double calc_poly(int n, double x) {
+
+    double x_pow = 1., sum = 0.;
+    for (int i = 1; i <= n; i++) {
+        x_pow *= x;
+        sum += i * x_pow;
+    }
+    return sum;
+}
+
+int main(void) {
+    int n;
+    double x;
+    scanf("%d%lf", &n, &x);
+
+    printf("P(%d, %lf) = %lf\n", n, x, calc_poly(n, x));
+}
