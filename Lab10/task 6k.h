f3ea85edffc2a93503229e8bd89292db98c9d8fc
Andriyrib <145540953+Andriyrib@users.noreply.github.com>
2023-12-13 15:30:54 +0200

    Protsan Andriy 2 year compmath1 lab10
    Classwork/Homework

---
+++ Lab10/task 6k.h
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_VERTICES 10  // Максимальна кількість вершин багатокутника
+
+// Структура для зберігання координат вершин багатокутника
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+// Структура для зберігання інформації про багатокутник
+typedef struct {
+    int size;  // Кількість вершин у багатокутнику
+    Point vertices[MAX_VERTICES];  // Масив вершин багатокутника
+} Polygon;
+
+// Функція для введення координат вершин багатокутника
+void inputPolygon(Polygon* polygon) {
+    printf("Enter the number of vertices of the polygon: ");
+    scanf("%d", &polygon->size);
+
+    printf("Enter the coordinates of the vertices in the format x y:\n");
+    for (int i = 0; i < polygon->size; ++i) {
+        scanf("%d %d", &polygon->vertices[i].x, &polygon->vertices[i].y);
+    }
+}
+
+// Функція для виведення координат вершин багатокутника
+void printPolygon(Polygon polygon) {
+    printf("Polygon with %d peaks:\n", polygon.size);
+    for (int i = 0; i < polygon.size; ++i) {
+        printf("(%d, %d)\n", polygon.vertices[i].x, polygon.vertices[i].y);
+    }
+}
+
+int main() {
+    Polygon myPolygon;
+    inputPolygon(&myPolygon);
+    printPolygon(myPolygon);
+
+    return 0;
+}
---
+++ Lab10/task10.h
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_COUNTRIES 50
+
+// Структура для зберігання інформації про державу
+typedef struct {
+    char name[50];
+    double population;  // Населення в мільйонах
+    double area;        // Площа
+} Country;
+
+// Функція для пошуку держави з мінімальною щільністю населення
+void findCountryWithMinDensity(Country countries[], int n) {
+    double minDensity = countries[0].population / countries[0].area;
+    int minIndex = 0;
+
+    for (int i = 1; i < n; ++i) {
+        double density = countries[i].population / countries[i].area;
+        if (density < minDensity) {
+            minDensity = density;
+            minIndex = i;
+        }
+    }
+
+    printf("The state with the minimum population density: %s\n", countries[minIndex].name);
+}
+
+int main() {
+    int numCountries;
+    printf("Enter the number of states: ");
+    scanf("%d", &numCountries);
+
+    if (numCountries <= 0 || numCountries > MAX_COUNTRIES) {
+        printf("Invalid value for number of states\n");
+        return 1;
+    }
+
+    Country countries[MAX_COUNTRIES];
+    printf("Enter data about states (name, population in millions, area):\n");
+    for (int i = 0; i < numCountries; ++i) {
+        printf("State %d: ", i + 1);
+        scanf("%s %lf %lf", countries[i].name, &countries[i].population, &countries[i].area);
+    }
+
+    findCountryWithMinDensity(countries, numCountries);
+
+    return 0;
+}
