048a17bd20e1fcada3fc5f14431db45b67d530ec
Andriyrib <145540953+Andriyrib@users.noreply.github.com>
2023-12-13 15:18:11 +0200

    protsan Andriy 2 year compmath1 lab1
    Classwork/Homework

---
+++ Lab1/task11.h
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    // Ініціалізація двох рядків
+    std::string str1 = "hello";
+    std::string str2 = "world!";
+
+    // Виведення рядків з розділенням комою та пробілом, та додавання трьох окличних знаків
+    std::cout << str1 << ", " << str2 << " !!!" << std::endl;
+
+    return 0;
+}
