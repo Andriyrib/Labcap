5e14519207fbb9233ce62adaa8f6f41865573616
Andriyrib <145540953+Andriyrib@users.noreply.github.com>
2023-12-13 15:32:24 +0200

    Protsan Andriy 2 year compmath1 lab12
    Classwork/Homework

---
+++ Lab12/task11.h
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+void processFile(FILE* file) {
+    double maxPrecision = 0.0;
+    double value;
+    char delimiter = ',';
+
+    // Пошук максимальної точності
+    while (fscanf(file, "%lf%c", &value, &delimiter) != EOF) {
+        int precision = 0;
+        char* ptr = strchr(strchr("%lf%c", '.') + 1, '.'); // Пошук другої крапки
+        if (ptr != NULL) {
+            precision = strlen(ptr + 1); // Знаходимо точність числа
+            if (precision > maxPrecision) {
+                maxPrecision = precision; // Оновлюємо максимальну точність
+            }
+        }
+    }
+
+    // Повертаємо позицію на початок файлу
+    fseek(file, 0, SEEK_SET);
+
+    // Заміна від'ємних чисел та запис до файлу
+    while (fscanf(file, "%lf%c", &value, &delimiter) != EOF) {
+        if (value < 0) {
+            fprintf(file, "%.lf", fabs(value)); // Запис модуля числа
+        }
+        else {
+            fprintf(file, "%.lf", value); // Запис додатного числа
+        }
+
+        if (delimiter == '\n' || delimiter == EOF) {
+            fprintf(file, "\n"); // Додавання роздільника рядку
+        }
+        else {
+            fprintf(file, ","); // Додавання коми
+        }
+    }
+}
+
+int main() {
+    FILE* file = fopen("input.txt", "r+");
+
+    if (file == NULL) {
+        printf("Помилка відкриття файлу.\n");
+        return 1;
+    }
+
+    processFile(file);
+
+    fclose(file);
+
+    return 0;
+}
---
+++ Lab12/task16.h
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+void processFile(FILE* fileF, FILE* fileG) {
+    int value;
+
+    while (fscanf(fileF, "%d", &value) != EOF) {
+        if (value > 0) {
+            double squareRoot = sqrt(value);
+            fprintf(fileG, "%d,%.5lf\n", value, squareRoot);
+        }
+    }
+}
+
+int main() {
+    FILE* fileF = fopen("F.txt", "r");
+    FILE* fileG = fopen("G.txt", "w");
+
+    if (fileF == NULL || fileG == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    processFile(fileF, fileG);
+
+    fclose(fileF);
+    fclose(fileG);
+
+    return 0;
+}
